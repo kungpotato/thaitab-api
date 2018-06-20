554710df533b9fa8f010d88cf8539c12b461f79c
kungpotato <kungPRS2014>
2018-06-21 01:11:49 +0700

    cors setting

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 8000,
-  mongoose = require('mongoose'),
-  Task = require('./api/models/todoListModel'), //created model loading here
+var express = require('express');
+var cors = require('cors');
+  app = express();
+  app.use(cors());
+  port = process.env.PORT || 8000;
+  mongoose = require('mongoose');
+  Task = require('./api/models/todoListModel'); //created model loading here
   bodyParser = require('body-parser');
   
 // mongoose instance connection url connection
@@ -13,6 +15,12 @@ mongoose.connect('mongodb://admin:kungPRS2008@ds163730.mlab.com:63730/db_kungpot
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});
 
 var routes = require('./api/routes/todoListRoutes'); //importing route
 routes(app); //register the route
